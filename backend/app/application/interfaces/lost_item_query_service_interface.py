d4bfd38a979017e69072bf062f8efb7498bc5784
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-03 19:51:12 -0300

    docs(LostItemQueryServiceInterface): Adiciona docstring para a interface

--- backend/app/application/interfaces/lost_item_query_service_interface.py
+++ backend/app/application/interfaces/lost_item_query_service_interface.py
@@ -4,5 +4,7 @@ from typing import Any
 
 class LostItemQueryServiceInterface(ABC):
 
+    """Interface do serviço de consulta de itens perdidos"""
+
     @abstractmethod
     def get_all_lost_items_summarized(self) -> list[dict[str, Any]]: ...
