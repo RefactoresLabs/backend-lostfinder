06d0f44b0131a118d544c4f242612804d78bb9a7
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-03 19:59:07 -0300

    feat(make_get_lost_item_details_controller): Implementa function factory que fornece o controller de obter detalhes de um item perdido

--- backend/app/presentation/controllers/factories/lost_item_factories.py
+++ backend/app/presentation/controllers/factories/lost_item_factories.py
@@ -1,17 +1,17 @@
 from backend.app.application.use_cases.list_lost_items_summarized_use_case import ListLostItemsSummarizedUseCase
-
-from backend.app.infrastructure.queries.lost_item_query_service import LostItemQueryService
+from backend.app.application.use_cases.create_lost_item_use_case import CreateLostItemUseCase
+from backend.app.application.use_cases.get_lost_item_details_use_case import GetLostItemDetailsUseCase
 
 from backend.app.presentation.controllers.list_lost_items_summarized_controller import ListLostItemsSummarizedController
-
-from backend.app.application.use_cases.create_lost_item_use_case import CreateLostItemUseCase
+from backend.app.presentation.controllers.create_lost_item_controller import CreateLostItemController
+from backend.app.presentation.controllers.get_lost_item_details_controller import GetLostItemDetailsController
 
 from backend.app.infrastructure.persistence.repositories.lost_item_repository import LostItemRepository
 from backend.app.infrastructure.persistence.repositories.category_repository import CategoryRepository
 from backend.app.infrastructure.persistence.repositories.building_space_repository import BuildingSpaceRepository
 from backend.app.infrastructure.persistence.repositories.user_account_repository import UserAccountRepository
+from backend.app.infrastructure.queries.lost_item_query_service import LostItemQueryService
 
-from backend.app.presentation.controllers.create_lost_item_controller import CreateLostItemController
 
 from sqlalchemy.orm import Session
 
@@ -68,3 +68,25 @@ def make_create_lost_item_controller(session: Session) -> CreateLostItemControll
     )
 
     return CreateLostItemController(create_lost_item_use_case)
+
+def make_get_lost_item_details_controller(session: Session) -> GetLostItemDetailsController:
+
+    """Factory function que cria um objeto GetLostItemDetailsController
+
+    Parameters
+    ----------
+    session: Session
+        Sessão usada para as transações com o banco
+    
+    Returns
+    -------
+    GetLostItemDetailsController
+        Ponto de acesso do endpoint com o caso de uso de obtenção de dados detalhados de um item perdido
+        
+    """
+
+    repository = LostItemRepository(session)
+
+    use_case = GetLostItemDetailsUseCase(repository)
+
+    return GetLostItemDetailsController(use_case)
