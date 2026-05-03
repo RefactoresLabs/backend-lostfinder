04d39b9f565ac248e2a5da581e72c3beaeb795fb
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-03 18:06:07 -0300

    test(test_list_lost_items_success): Verifica situação em que o caso de uso de listar itens perdidos resumidamente obtem sucesso

--- backend/app/tests/application/use_cases/test_list_lost_items_summarized_use_case.py
+++ backend/app/tests/application/use_cases/test_list_lost_items_summarized_use_case.py
@@ -1,6 +1,6 @@
 from unittest.mock import Mock
 
-from backend.app.application.dtos.list_lost_items_summarized_dto import ListLostItemsSummarizedDTO
+from backend.app.application.dtos.list_items_summarized_dto import ListItemsSummarizedDTO
 from backend.app.application.use_cases.list_lost_items_summarized_use_case import ListLostItemsSummarizedUseCase
 
 def test_list_lost_items_use_case_success():
@@ -28,7 +28,7 @@ def test_list_lost_items_use_case_success():
 
     use_case = ListLostItemsSummarizedUseCase(query_service)
 
-    dtos: list[ListLostItemsSummarizedDTO] = use_case.execute()
+    dtos: list[ListItemsSummarizedDTO] = use_case.execute()
 
     assert dtos[0].item_name == "Lápis"
     assert dtos[1].image_url is None
