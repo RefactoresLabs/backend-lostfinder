a9490d87b1640e5a819ecef19026541835fc5c84
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-03 19:48:24 -0300

    refactor: Alterando exceção ValueError para exceções mais específicas

--- backend/app/application/use_cases/create_found_item_use_case.py
+++ backend/app/application/use_cases/create_found_item_use_case.py
@@ -4,6 +4,9 @@ from backend.app.domain.repositories.building_space_repository_interface import 
 from backend.app.domain.repositories.user_account_repository_interface import UserAccountRepositoryInterface
 from backend.app.domain.entities.found_item import FoundItem
 from backend.app.domain.value_objects.image import Image
+from backend.app.domain.exceptions.category_doesnt_exist_error import CategoryDoesntExistError
+from backend.app.domain.exceptions.building_space_doesnt_exist_error import BuildingSpaceDoesntExistError
+from backend.app.domain.exceptions.user_account_doesnt_exist_error import UserAccountDoesntExistError
 
 from backend.app.application.dtos.create_found_item_dto import CreateFoundItemDTO
 
@@ -43,7 +46,7 @@ class CreateFoundItemUseCase:
         self.__building_space_repository = building_space_repository
         self.__user_account_repository = user_account_repository
 
-    def execute(self, dto: CreateFoundItemDTO) -> dict:
+    def execute(self, dto: CreateFoundItemDTO) -> None:
 
         """Executa o fluxo de eventos do caso de uso criar item encontrado
 
@@ -52,41 +55,42 @@ class CreateFoundItemUseCase:
         dto: CreateFoundItemDTO
             Objeto de transferência de dados com as informações do item encontrado
 
-        Returns
-        -------
-        dict
-            Dicionário com os dados do item encontrado registrado
-
         Raises
         ------
-        ValueError
-            Se a categoria, espaço do prédio ou conta de usuário não forem encontrados
-
+        CategoryDoesntExistError
+            Exceção levantada quando a categoria não é encontrada
+        
+        BuildingSpaceDoesntExistError
+            Exceção levantada quando o espaço do prédio não é encontrado
+        
+        UserAccountDoesntExistError
+            Exceção levantada quando a conta de usuário não é encontrada
+            
         """
 
         category = self.__category_repository.get_category_by_id(dto.category_id)
 
         if category is None:
 
-            raise ValueError("Categoria não encontrada")
+            raise CategoryDoesntExistError("Categoria não encontrada")
 
         found_building_space = self.__building_space_repository.get_building_space_by_id(dto.found_building_space_id)
 
         if found_building_space is None:
 
-            raise ValueError("Espaço do prédio onde o item foi encontrado não encontrado")
+            raise BuildingSpaceDoesntExistError("Espaço do prédio onde o item foi encontrado não encontrado")
 
         left_building_space = self.__building_space_repository.get_building_space_by_id(dto.left_building_space_id)
 
         if left_building_space is None:
 
-            raise ValueError("Espaço do prédio onde o item foi deixado não encontrado")
+            raise BuildingSpaceDoesntExistError("Espaço do prédio onde o item foi deixado não encontrado")
 
         user_account = self.__user_account_repository.get_user_account_by_id(dto.user_id)
 
         if user_account is None:
 
-            raise ValueError("Conta de usuário não encontrada")
+            raise UserAccountDoesntExistError("Conta de usuário não encontrada")
 
         images = [Image(url=url) for url in dto.image_urls]
 
@@ -101,13 +105,4 @@ class CreateFoundItemUseCase:
             approx_left_building_space=left_building_space,
         )
 
-        created_found_item = self.__found_item_repository.create_new_found_item(found_item)
-
-        return {
-            "id": created_found_item.id,
-            "name": created_found_item.name,
-            "description": created_found_item.description,
-            "category": created_found_item.category.name,
-            "found_building_space": created_found_item.approx_found_building_space.name,
-            "left_building_space": created_found_item.approx_left_building_space.name,
-        }
+        _ = self.__found_item_repository.create_new_found_item(found_item)
