402b2c55b7fc35aa6c9b89277b37751aa9eac4fb
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-03 13:37:44 -0300

    docs(UserAccountRepositoryInterface): Define a docstring da interface

--- backend/app/domain/repositories/user_account_repository_interface.py
+++ backend/app/domain/repositories/user_account_repository_interface.py
@@ -2,16 +2,19 @@ from abc import ABC, abstractmethod
 
 from backend.app.domain.entities.user_account import UserAccount
 
+
 class UserAccountRepositoryInterface(ABC):
 
+    """Interface do repositório da entidade UserAccount"""
+
     @abstractmethod
-    def create_new_user_account(self, user_account: UserAccount) -> UserAccount: ...
+    def create_new_user_account(self, user_account: UserAccount) -> UserAccount | None: ...
 
     @abstractmethod
     def update_user_account(self, user_account: UserAccount, id: int) -> UserAccount: ...
 
     @abstractmethod
-    def get_user_account_by_id(self, id: int) -> UserAccount: ...
+    def get_user_account_by_id(self, id: int) -> UserAccount | None: ...
 
     @abstractmethod
     def get_user_account_by_email(self, email: str) -> UserAccount | None: ...
