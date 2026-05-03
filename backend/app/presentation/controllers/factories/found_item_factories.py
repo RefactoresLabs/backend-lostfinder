8e5b0d9b5abd57410aa3c4f136cee5c69a56b884
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-03 19:58:42 -0300

    feat(make_get_found_item_details_controller): Implementa function factory que fornece o controller de obter detalhes de um item encontrado

--- backend/app/presentation/controllers/factories/found_item_factories.py
+++ backend/app/presentation/controllers/factories/found_item_factories.py
@@ -1,14 +1,40 @@
 from backend.app.application.use_cases.create_found_item_use_case import CreateFoundItemUseCase
+from backend.app.application.use_cases.list_found_items_summarized_use_case import ListFoundItemsSummarizedUseCase
+from backend.app.application.use_cases.get_found_item_details_use_case import GetFoundItemDetailsUseCase
 
 from backend.app.infrastructure.persistence.repositories.found_item_repository import FoundItemRepository
 from backend.app.infrastructure.persistence.repositories.category_repository import CategoryRepository
 from backend.app.infrastructure.persistence.repositories.building_space_repository import BuildingSpaceRepository
 from backend.app.infrastructure.persistence.repositories.user_account_repository import UserAccountRepository
+from backend.app.infrastructure.queries.found_item_query_service import FoundItemQueryService
 
 from backend.app.presentation.controllers.create_found_item_controller import CreateFoundItemController
+from backend.app.presentation.controllers.list_found_items_summarized_controller import ListFoundItemsSummarizedController
+from backend.app.presentation.controllers.get_found_item_details_controller import GetFoundItemDetailsController
 
 from sqlalchemy.orm import Session
 
+def make_list_found_item_summarized_controller(session: Session):
+
+    """Factory function que cria um objeto ListFoundItemsSummarizedController
+
+    Parameters
+    ----------
+    session: Session
+        Sessão usada para as transações com o banco
+    
+    Returns
+    -------
+    ListFoundItemsSummarizedController
+        Ponto de acesso do endpoint com o caso de uso de listar itens encontrados resumidamente
+        
+    """
+
+    query_service = FoundItemQueryService(session)
+
+    use_case = ListFoundItemsSummarizedUseCase(query_service)
+
+    return ListFoundItemsSummarizedController(use_case)
 
 def make_create_found_item_controller(session: Session) -> CreateFoundItemController:
 
@@ -39,3 +65,25 @@ def make_create_found_item_controller(session: Session) -> CreateFoundItemContro
     )
 
     return CreateFoundItemController(create_found_item_use_case)
+
+def make_get_found_item_details_controller(session: Session) -> GetFoundItemDetailsController:
+
+    """Factory function que cria um objeto GetFoundItemDetailsController
+
+    Parameters
+    ----------
+    session: Session
+        Sessão usada para as transações com o banco
+    
+    Returns
+    -------
+    GetFoundItemDetailsController
+        Ponto de acesso do endpoint com o caso de uso de obtenção de dados detalhados de um item encontrado
+        
+    """
+
+    repository = FoundItemRepository(session)
+
+    use_case = GetFoundItemDetailsUseCase(repository)
+
+    return GetFoundItemDetailsController(use_case)
