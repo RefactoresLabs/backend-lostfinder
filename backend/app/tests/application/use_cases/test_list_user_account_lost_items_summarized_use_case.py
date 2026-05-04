8d97b27f8cec1dc57cefce776af4956e25daea34
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-03 22:34:20 -0300

    test(test_list_lost_items_use_case_success): Verifica situação em que o caso de uso de listar itens perdidos de um usuário específico é executado corretamente

---
+++ backend/app/tests/application/use_cases/test_list_user_account_lost_items_summarized_use_case.py
@@ -0,0 +1,39 @@
+from unittest.mock import Mock
+
+from backend.app.application.dtos.list_items_summarized_dto import ListItemsSummarizedDTO
+from backend.app.application.dtos.list_user_account_items_summarized_input_dto import ListUserAccountItemsSummarizedInputDTO
+from backend.app.application.use_cases.list_user_account_lost_items_summarized_use_case import ListUserAccountLostItemsSummarizedUseCase
+
+def test_list_lost_items_use_case_success():
+
+    query_service = Mock()
+
+    query_service.get_lost_items_summarized_by_user_id.return_value = [
+        {
+            "item_id": 1,
+            "item_name": "Lápis",
+            "user_name": "Link",
+            "category_name": "Material",
+            "building_space_name": "sala 1",
+            "image_url": "./storage/image1.png",
+        },
+        {
+            "item_id": 2,
+            "item_name": "Caneta",
+            "user_name": "Mario",
+            "category_name": "Material",
+            "building_space_name": "sala 2",
+            "image_url": None,
+        },
+    ]
+
+    use_case = ListUserAccountLostItemsSummarizedUseCase(query_service)
+
+    input_dto = ListUserAccountItemsSummarizedInputDTO(
+        user_id=1
+    )
+
+    dtos: list[ListItemsSummarizedDTO] = use_case.execute(input_dto)
+
+    assert dtos[0].item_name == "Lápis"
+    assert dtos[1].image_url is None
