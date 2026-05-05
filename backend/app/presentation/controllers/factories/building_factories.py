0a2af1ba51c31437e1b59c9d4a44a69faa25ca61
Seu Nome <seu@email.com>
2026-05-05 09:59:26 -0300

    feat(building_factories): Implementa factory do controller de coordenadas

---
+++ backend/app/presentation/controllers/factories/building_factories.py
@@ -0,0 +1,35 @@
+from backend.app.application.use_cases.get_building_coordinates_use_case import GetBuildingCoordinatesUseCase
+
+from backend.app.infrastructure.persistence.repositories.building_repository import BuildingRepository
+from backend.app.infrastructure.external.maps.nominatim_geocode_adapter import NominatimGeocodeAdapter
+
+from backend.app.presentation.controllers.get_building_coordinates_controller import GetBuildingCoordinatesController
+
+from sqlalchemy.orm import Session
+
+
+def make_get_building_coordinates_controller(session: Session) -> GetBuildingCoordinatesController:
+
+    """Factory function que cria um objeto GetBuildingCoordinatesController
+
+    Parameters
+    ----------
+    session: Session
+        Sessão usada para as transações com o banco
+
+    Returns
+    -------
+    GetBuildingCoordinatesController
+        Ponto de acesso do endpoint com o caso de uso de obter coordenadas do prédio
+
+    """
+
+    building_repository = BuildingRepository(session)
+    geocode_service = NominatimGeocodeAdapter()
+
+    get_building_coordinates_use_case = GetBuildingCoordinatesUseCase(
+        building_repository=building_repository,
+        geocode_service=geocode_service,
+    )
+
+    return GetBuildingCoordinatesController(get_building_coordinates_use_case)
