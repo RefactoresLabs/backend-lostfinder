9b041d63260de0f85af632c57aa88bed0a39eac4
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-08 00:37:03 -0300

    feat(ListBuildingSpacesUseCase.execute): Implementa exceção levantada quando um prédio não é econtrado

--- backend/app/application/use_cases/list_building_spaces_use_case.py
+++ backend/app/application/use_cases/list_building_spaces_use_case.py
@@ -1,4 +1,6 @@
 from backend.app.domain.repositories.building_space_repository_interface import BuildingSpaceRepositoryInterface
+from backend.app.domain.repositories.building_repository_interface import BuildingRepositoryInterface
+from backend.app.domain.exceptions.building_doesnt_exist_error import BuildingDoesntExistError
 
 from backend.app.application.dtos.list_building_spaces_input_dto import ListBuildingSpacesInputDTO
 from backend.app.application.dtos.list_building_spaces_output_dto import ListBuildingSpacesOutputDTO
@@ -7,7 +9,7 @@ class ListBuildingSpacesUseCase:
 
     """Representa um caso de uso de listar espaços de um prédio"""
 
-    def __init__(self, building_space_repository: BuildingSpaceRepositoryInterface) -> None:
+    def __init__(self, building_space_repository: BuildingSpaceRepositoryInterface, building_repository: BuildingRepositoryInterface) -> None:
 
         """Inicializa os atributos de instância de ListBuildingSpacesUseCase
 
@@ -15,10 +17,14 @@ class ListBuildingSpacesUseCase:
         ----------
         building_space_repository: BuildingSpaceRepositoryInterface
             Repositório de espaço de prédio para busca de dados
+        
+        building_repository: BuildingRepositoryInterface
+            Repositório de prédio para checar sua existência
 
         """
 
-        self.__repository = building_space_repository
+        self.__building_space_repository = building_space_repository
+        self.__building_repository = building_repository
     
     def execute(self, dto: ListBuildingSpacesInputDTO) -> list[ListBuildingSpacesOutputDTO]:
 
@@ -33,10 +39,21 @@ class ListBuildingSpacesUseCase:
         -------
         list[ListBuildingSpacesOutputDTO]
             Objetos de transferência de dados com os dados dos espaços do prédio
+        
+        Raises
+        ------
+        BuildingDoesntExistError
+            Levantado quando o prédio não é encontrado
 
         """
 
-        building_spaces = self.__repository.get_building_spaces_by_building_id(dto.building_id)
+        building = self.__building_repository.get_buildind_by_id(dto.building_id)
+
+        if not building:
+
+            raise BuildingDoesntExistError("Prédio não encontrado")
+
+        building_spaces = self.__building_space_repository.get_building_spaces_by_building_id(dto.building_id)
 
         return [
             ListBuildingSpacesOutputDTO(
