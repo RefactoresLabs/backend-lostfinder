662e4d2c2b17c2cba43a5f903af5c4a6e63cec0e
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-17 00:29:45 -0300

    feat(UserAccount.score): Adiciona atributo de pontuação a entidade de conta de usuário do negócio

--- backend/app/domain/entities/user_account.py
+++ backend/app/domain/entities/user_account.py
@@ -2,7 +2,7 @@ class UserAccount:
 
     """Representa a entidade conta de usuário da regra de negócio"""
 
-    def __init__(self, id: int | None, name: str, email: str, password: str, phone: str) -> None:
+    def __init__(self, id: int | None, name: str, email: str, password: str, phone: str, score: int=0) -> None:
 
         """Inicializa os atributos de instância de UserAccount
 
@@ -23,8 +23,9 @@ class UserAccount:
         phone: str
             Telefone do usuário
 
-        id: int | None
-            Identificador único da conta. Opcional (gerado pelo banco)
+        score: int (Padrão: 0)
+            Pontuação da conta de usuário
+
         """
 
         self.__id = id
@@ -32,6 +33,7 @@ class UserAccount:
         self.__email = email
         self.__password = password
         self.__phone = phone
+        self.__score = score
     
     @property
     def id(self) -> int | None:
@@ -103,6 +105,20 @@ class UserAccount:
 
         return self.__phone
     
+    @property
+    def score(self) -> int:
+
+        """Obtém a pontuação da conta de usuário
+
+        Returns
+        -------
+        int
+            Pontuação da conta de usuário
+
+        """
+
+        return self.__score
+    
     def _set_id(self, id: int) -> None:
 
         """Modifica o valor do atributo id da conta de usuário uma única vez, caso ela seja None no estado atual
@@ -119,3 +135,25 @@ class UserAccount:
             raise ValueError("Valor de ID já definido!")
         
         self.__id = id
+    
+    def add_score(self, points: int) -> None:
+
+        """Adiciona pontos a pontuação da conta de usuário
+
+        Parameters
+        ----------
+        points: int
+            Pontos a serem adicionados
+        
+        Raises
+        ------
+        ValueError
+            Exceção levantada quando a pontuação passada for negativa
+
+        """
+
+        if points < 0:
+
+            ValueError("Os pontos não podem ser negativos")
+        
+        self.__score += points
