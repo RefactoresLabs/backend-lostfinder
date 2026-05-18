c932a61629ba4c42210227f1c4d445ec7fc43f7c
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-17 23:38:24 -0300

    fix(CurrentClaimStatusError): Corrige problema do construtor da exceção não poder receber argumentos

--- backend/app/domain/exceptions/current_claim_status_error.py
+++ backend/app/domain/exceptions/current_claim_status_error.py
@@ -1,4 +1,4 @@
-class CurrentClaimStatusError:
+class CurrentClaimStatusError(Exception):
 
     """Exceção levantada quando o status da negociação a ser definido não é válido, de acordo com o status de negócio atual"""
 
