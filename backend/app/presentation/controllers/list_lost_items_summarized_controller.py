3578f889f4074a401bc9e594f6cc199ee90cfb98
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-19 15:46:51 -0300

    feat(ListLostItemsSummarizedController.handle): Implementa parametros na requisição HTTP de filtros e ordenamento

--- backend/app/presentation/controllers/list_lost_items_summarized_controller.py
+++ backend/app/presentation/controllers/list_lost_items_summarized_controller.py
@@ -1,5 +1,5 @@
 from backend.app.application.use_cases.list_lost_items_summarized_use_case import ListLostItemsSummarizedUseCase
-
+from backend.app.application.dtos.list_items_summarized_input_dto import ListItemsSummarizedInputDTO
 
 from backend.app.presentation.schemas.http_response import HttpResponse
 from backend.app.presentation.schemas.http_request import HttpRequest
@@ -41,7 +41,19 @@ class ListLostItemsSummarizedController:
 
         limit = http_request.params.get("limit", 0)
 
-        dtos = self.__use_case.execute()
+        name = http_request.params.get("name")
+        category_id = http_request.params.get("category_id")
+        sort_by = http_request.params.get("sort_by", "name")
+        sort_option = http_request.params.get("sort_option", "asc")
+
+        dto = ListItemsSummarizedInputDTO(
+            name,
+            category_id,
+            sort_by,
+            sort_option
+        )
+
+        dtos = self.__use_case.execute(dto)
 
         if limit < 0:
 
