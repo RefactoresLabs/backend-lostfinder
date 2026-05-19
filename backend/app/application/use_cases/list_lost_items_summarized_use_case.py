a7fdddf2512b7c4173ce828075a13823485e0275
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-19 15:42:05 -0300

    feat(ListLostItemsSummarizedUseCase.execute): Implementa possibilidade de aplicar filtros e ordenamento nos itens perdidos resumidos

--- backend/app/application/use_cases/list_lost_items_summarized_use_case.py
+++ backend/app/application/use_cases/list_lost_items_summarized_use_case.py
@@ -1,5 +1,12 @@
 from backend.app.application.interfaces.lost_item_query_service_interface import LostItemQueryServiceInterface
-from backend.app.application.dtos.list_items_summarized_dto import ListItemsSummarizedDTO
+from backend.app.application.dtos.list_items_summarized_output_dto import ListItemsSummarizedOutputDTO
+from backend.app.application.dtos.list_items_summarized_input_dto import ListItemsSummarizedInputDTO
+
+from backend.app.application.queries.filters.list_items_summary_filters import ListItemsSummaryFilters
+from backend.app.application.queries.sorts.list_items_summary_sort import ListItemsSummarySort
+from backend.app.application.queries.sorts.enums.list_items_summary_sort_field import ListItemsSummarySortField
+from backend.app.application.queries.sorts.enums.list_items_summary_sort_option import ListItemsSummarySortOption
+
 
 class ListLostItemsSummarizedUseCase:
 
@@ -19,21 +26,32 @@ class ListLostItemsSummarizedUseCase:
 
         self.__query_service = lost_item_query_service
     
-    def execute(self) -> list[ListItemsSummarizedDTO]:
+    def execute(self, dto: ListItemsSummarizedInputDTO) -> list[ListItemsSummarizedOutputDTO]:
 
         """Executa o fluxo de eventos do caso de uso de listar itens perdidos resumidamente
 
+        Parameters
+        ----------
+        dto: ListItemsSummarizedInputDTO
+            Objeto de transferência de dados contendo dados de filtro e ordenamento
+
         Returns
         -------
-        list[ListItemsSummarizedDTO]
+        list[ListItemsSummarizedOutputDTO]
             Iterável contendo os DTOs com os dados resumidos dos itens perdidos
             
         """
 
-        summarized_lost_items = self.__query_service.get_all_lost_items_summarized()
+        filters = ListItemsSummaryFilters(dto.filter_name, dto.filter_category_id)
+        sort = ListItemsSummarySort(
+            ListItemsSummarySortField(dto.sort_by), 
+            ListItemsSummarySortOption(dto.sort_option),
+        )
+
+        summarized_lost_items = self.__query_service.get_all_lost_items_summarized(filters, sort)
 
         return [
-            ListItemsSummarizedDTO(
+            ListItemsSummarizedOutputDTO(
                 lost_item["item_id"],
                 lost_item["item_name"],
                 lost_item["user_name"],
