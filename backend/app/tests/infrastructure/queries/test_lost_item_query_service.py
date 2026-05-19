3bb20479acb994f876c95b939ccbdc51c0d53ca8
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-19 15:53:49 -0300

    test: Verifica situações em que filtros e ordenamentos são aplicados aos itens listados resumidamente

--- backend/app/tests/infrastructure/queries/test_lost_item_query_service.py
+++ backend/app/tests/infrastructure/queries/test_lost_item_query_service.py
@@ -16,6 +16,11 @@ from backend.app.infrastructure.persistence.models.lost_item_model import LostIt
 from backend.app.infrastructure.persistence.models.item_model import ItemModel
 from backend.app.infrastructure.persistence.models.base import Base
 
+from backend.app.application.queries.filters.list_items_summary_filters import ListItemsSummaryFilters
+from backend.app.application.queries.sorts.list_items_summary_sort import ListItemsSummarySort
+from backend.app.application.queries.sorts.enums.list_items_summary_sort_field import ListItemsSummarySortField
+from backend.app.application.queries.sorts.enums.list_items_summary_sort_option import ListItemsSummarySortOption
+
 
 @pytest.fixture
 def session():
@@ -113,6 +118,38 @@ def seed_data(session: Session):
         user_id=1,
     )
 
+    item4 = ItemModel(
+        id=4,
+        name="Borracha",
+        description="Borracha branca",
+        category_id=2,
+        user_id=1,
+    )
+
+    item5 = ItemModel(
+        id=5,
+        name="Carteira",
+        description="Carteira Preta",
+        category_id=1,
+        user_id=1,
+    )
+
+    item6 = ItemModel(
+        id=6,
+        name="Fone",
+        description="Fone JBL Branco",
+        category_id=1,
+        user_id=1,
+    )
+
+    item7 = ItemModel(
+        id=7,
+        name="Borracha",
+        description="Borracha preta",
+        category_id=1,
+        user_id=1,
+    )
+
     lost_item1 = LostItemModel(
         id=1,
         lost_space_id=1,
@@ -125,6 +162,26 @@ def seed_data(session: Session):
 
     lost_item3 = LostItemModel(
         id=3,
+        lost_space_id=3,
+    )
+
+    lost_item4 = LostItemModel(
+        id=4,
+        lost_space_id=3,
+    )
+    
+    lost_item5 = LostItemModel(
+        id=5,
+        lost_space_id=3,
+    )
+
+    lost_item6 = LostItemModel(
+        id=6,
+        lost_space_id=3,
+    )
+
+    lost_item7 = LostItemModel(
+        id=7,
         lost_space_id=3,
     )
 
@@ -146,9 +203,17 @@ def seed_data(session: Session):
         item1,
         item2,
         item3,
+        item4,
+        item5,
+        item6,
+        item7,
         lost_item1,
         lost_item2,
         lost_item3,
+        lost_item4,
+        lost_item5,
+        lost_item6,
+        lost_item7,
         image
     ])
 
@@ -158,12 +223,65 @@ def test_get_all_lost_item_summarized_success(session: Session, seed_data):
 
     query_service = LostItemQueryService(session)
 
-    lost_items_summarized = query_service.get_all_lost_items_summarized()
+    filters = ListItemsSummaryFilters(None, None)
+    sort = ListItemsSummarySort(ListItemsSummarySortField.NAME, ListItemsSummarySortOption.ASC)
+
+    lost_items_summarized = query_service.get_all_lost_items_summarized(filters, sort)
+
+    assert len(lost_items_summarized) == 7
+    assert lost_items_summarized[0]["item_name"] == "Borracha"
+    assert lost_items_summarized[1]["item_name"] == "Borracha"
+
+def test_get_all_lost_items_summarized_filter_name(session: Session, seed_data):
+
+    query_service = LostItemQueryService(session)
+
+    filters = ListItemsSummaryFilters("Borracha", None)
+    sort = ListItemsSummarySort(ListItemsSummarySortField.NAME, ListItemsSummarySortOption.ASC)
+
+    lost_items_summarized = query_service.get_all_lost_items_summarized(filters, sort)
 
     assert len(lost_items_summarized) == 3
+    assert lost_items_summarized[0]["item_name"] == "Borracha"
+    assert lost_items_summarized[1]["item_name"] == "Borracha"
+
+def test_get_all_lost_items_summarized_filter_category_id(session: Session, seed_data):
+
+    query_service = LostItemQueryService(session)
+
+    filters = ListItemsSummaryFilters(None, 1)
+    sort = ListItemsSummarySort(ListItemsSummarySortField.NAME, ListItemsSummarySortOption.ASC)
+
+    lost_items_summarized = query_service.get_all_lost_items_summarized(filters, sort)
+
+    assert len(lost_items_summarized) == 3
+    assert lost_items_summarized[0]["item_name"] == "Borracha"
+    assert lost_items_summarized[1]["item_name"] == "Carteira"
+    assert lost_items_summarized[2]["item_name"] == "Fone"
+
+def test_get_all_lost_items_summarized_filter_name_and_category_id(session: Session, seed_data):
+
+    query_service = LostItemQueryService(session)
+
+    filters = ListItemsSummaryFilters("Borracha", 1)
+    sort = ListItemsSummarySort(ListItemsSummarySortField.NAME, ListItemsSummarySortOption.ASC)
+
+    lost_items_summarized = query_service.get_all_lost_items_summarized(filters, sort)
+    
+    assert len(lost_items_summarized) == 1
+    assert lost_items_summarized[0]["item_name"] == "Borracha"
+
+def test_get_all_lost_items_summarized_name_desc_sort_option(session: Session, seed_data):
+
+    query_service = LostItemQueryService(session)
+
+    filters = ListItemsSummaryFilters(None, None)
+    sort = ListItemsSummarySort(ListItemsSummarySortField.NAME, ListItemsSummarySortOption.DESC)
+
+    lost_items_summarized = query_service.get_all_lost_items_summarized(filters, sort)
+
+    assert len(lost_items_summarized) == 7
     assert lost_items_summarized[0]["item_name"] == "Lápis"
-    assert lost_items_summarized[1]["building_space_name"] == "sala 2"
-    assert lost_items_summarized[2]["image_url"] is None
 
 def test_get_lost_items_summarized_by_user_id_success(session: Session, seed_data):
 
