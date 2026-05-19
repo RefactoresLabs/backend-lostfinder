6f0fffa368f8d1a8467fdb3edfbb3cfc86f82b48
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-18 23:15:26 -0300

    feat(get_user_account_details): Implementa operação get de obter os detalhes de um usuário específico

--- backend/app/presentation/routers/user_account_routes.py
+++ backend/app/presentation/routers/user_account_routes.py
@@ -6,7 +6,11 @@ import os
 
 
 from backend.app.presentation.schemas.http_request import HttpRequest
-from backend.app.presentation.controllers.factories.user_account_factories import make_register_user_account_controller
+from backend.app.presentation.controllers.factories.user_account_factories import (
+    make_register_user_account_controller,
+    make_get_user_account_details_controller,
+)
+from backend.app.presentation.middlewares.jwt_required import jwt_required
 
 from backend.app.infrastructure.database.session_manager import SessionManager
 from backend.app.infrastructure.database.database_url_builder import DatabaseURLBuilder
@@ -40,6 +44,37 @@ def create_user_account_routes(app: Flask) -> None:
             )
 
             http_response = register_user_account_controler.handle(http_request)
+        
+        return jsonify(http_response.body), http_response.status_code
+
+    @app.route("/users", methods=["GET"])
+    @jwt_required
+    def get_user_account_details():
+
+        database_url = DatabaseURLBuilder.build(
+            os.environ["SGBD"],
+            {
+                "DATABASE": os.environ.get("DATABASE"),
+                "USERNAME": os.environ.get("USERNAME"),
+                "PASSWORD": os.environ.get("PASSWORD"),
+                "HOSTNAME": os.environ.get("HOSTNAME"),
+                "DATABASE_PORT": os.environ.get("DATABASE_PORT"),
+            },
+        )
+
+        with SessionManager(database_url) as session_manager:
+
+            http_request = HttpRequest(
+                params={
+                    "user_id": request.user_payload["user_id"],
+                }
+            )
+
+            get_user_account_details_controller = make_get_user_account_details_controller(
+                session_manager.session
+            )
+
+            http_response = get_user_account_details_controller.handle(http_request)
         
         return jsonify(http_response.body), http_response.status_code
 
