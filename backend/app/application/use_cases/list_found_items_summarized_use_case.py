c9574c26a62a73651d33566e719afd973f2c892d
LucasGPestana <lucasgabrielpestana03@gmail.com>
2026-05-19 15:41:46 -0300

    feat(ListFoundItemsSummarizedUseCase.execute): Implementa possibilidade de aplicar filtros e ordenamento nos itens encontrados resumidos

--- backend/app/application/use_cases/list_found_items_summarized_use_case.py
+++ backend/app/application/use_cases/list_found_items_summarized_use_case.py
@@ -1,5 +1,12 @@
 from backend.app.application.interfaces.found_item_query_service_interface import FoundItemQueryServiceInterface
-from backend.app.application.dtos.list_items_summarized_dto import ListItemsSummarizedDTO
+from backend.app.application.dtos.list_items_summarized_output_dto import ListItemsSummarizedOutputDTO
+from backend.app.application.dtos.list_items_summarized_input_dto import ListItemsSummarizedInputDTO
+
+from backend.app.application.queries.filters.list_items_summary_filters import ListItemsSummaryFilters
+from backend.app.application.queries.sorts.list_items_summary_sort import ListItemsSummarySort
+from backend.app.application.queries.sorts.enums.list_items_summary_sort_field import ListItemsSummarySortField
+from backend.app.application.queries.sorts.enums.list_items_summary_sort_option import ListItemsSummarySortOption
+
 
 class ListFoundItemsSummarizedUseCase:
 
@@ -19,21 +26,30 @@ class ListFoundItemsSummarizedUseCase:
 
         self.__query_service = found_item_query_service
     
-    def execute(self) -> list[ListItemsSummarizedDTO]:
+    def execute(self, dto: ListItemsSummarizedInputDTO) -> list[ListItemsSummarizedOutputDTO]:
 
         """Executa o fluxo de eventos do caso de uso de listar itens encontrados resumidamente
 
+        dto: ListItemsSummarizedInputDTO
+            Objeto de transferência de dados contendo dados de filtro e ordenamento
+
         Returns
         -------
-        list[ListItemsSummarizedDTO]
+        list[ListItemsSummarizedOutputDTO]
             Iterável contendo os DTOs com os dados resumidos dos itens encontrados
             
         """
 
-        summarized_found_items = self.__query_service.get_all_found_items_summarized()
+        filters = ListItemsSummaryFilters(dto.filter_name, dto.filter_category_id)
+        sort = ListItemsSummarySort(
+            ListItemsSummarySortField(dto.sort_by), 
+            ListItemsSummarySortOption(dto.sort_option),
+        )
+
+        summarized_found_items = self.__query_service.get_all_found_items_summarized(filters, sort)
 
         return [
-            ListItemsSummarizedDTO(
+            ListItemsSummarizedOutputDTO(
                 found_item["item_id"],
                 found_item["item_name"],
                 found_item["user_name"],
